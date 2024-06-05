98f1164278ba3fbdc81bd44e24a8b93ed54750a7
tom.frumy <tom.frumy@etu.univ-lyon1.fr>
2024-06-05 16:02:35 +0200

    ok

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,7 +17,7 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             Fenetre fenetre = new Fenetre();
             fenetre.setVisible(true);
-            fenetre.afficherGraphes(graphes, 1);
+            fenetre.afficherGraphes(graphes);
         });
     }
 }
